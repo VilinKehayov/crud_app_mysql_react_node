9f06cc2fd942661988f09f6ebf71a9bf7d685b32
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-04-19 11:57:56 +0300

    update page made, useLocation hook added.

--- client/src/pages/Update.jsx
+++ client/src/pages/Update.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, {useState} from "react";
+import axios from "axios"
+import {useNavigate, useLocation} from "react-router-dom"
 
-const Update = () => {
+const Update = () => { 
+    
+    const [input, setInput] = useState({
+        title: "",
+        desc: "",
+        price: null, 
+        cover: "",
+    })
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    
+    const bookId = location.pathname.split("/")[2]
+    
+    const handleChange = (e) => {
+        setInput(prev => ({...prev,[e.target.name]: e.target.value}))
+        console.log(input);
+    }
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+        try {
+           await axios.put("http://localhost:3000/books"+bookId, input)
+            navigate("/")
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
   return (
-    <div>Update</div>
-  )
-}
+    <div className="form">
+      <h1>Update the book</h1>
+      <input type="text" placeholder="title" onChange={handleChange} name="title"/>
+      <input type="text" placeholder="desc" onChange={handleChange} name="desc"/>
+      <input type="number" placeholder="price" onChange={handleChange} name="number"/>
+      <input type="text" placeholder="cover"onChange={handleChange}  name="cover"/>
+      <button className="formButton" onClick={handleClick}>Update</button>
+    </div>
+  );
+};
 
-export default Update
+export default Update;
