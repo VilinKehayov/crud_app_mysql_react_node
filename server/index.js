33bd8ca625faab3d6664b064fa3fef3a5401d34a
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-04-19 10:24:09 +0300

    cors, post req, price column

--- server/index.js
+++ server/index.js
@@ -1,43 +1,47 @@
-import express from "express"
-import mysql from "mysql"
-import cors from "cors"
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
 
 const app = express();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "mysql123",
-    database: "crud_app"
-})
+  host: "localhost",
+  user: "root",
+  password: "mysql123",
+  database: "crud_app",
+});
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/",(req, res)=>{
-    res.json("Hello are you there?")
-})
+app.get("/", (req, res) => {
+  res.json("Hello are you there?");
+});
 
-app.get("/books",(req,res)=>{
-    const q = "SELECT * FROM books"
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
 
-    db.query(q,(err,data)=>{
-        if(err) return res.json(err)
-        return res.json(data)
-    })
-})
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
-app.post("/books", (req,res)=>{
-    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
-    const values = [req.body.title, req.body.desc, req.body.cover,];
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`, `desc`,`price`, `cover`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
 
-    db.query(q, [values], (err,data)=>{
-        if(err) return res.json(err);
-        return res.json("It works");
-    })
-})
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("It works");
+  });
+});
 
-
-app.listen(3000, ()=>{
-    console.log("Server connected!")
-})
+app.listen(3000, () => {
+  console.log("Server connected!");
+});
