fbf6a4528be4be33c9e0844c3ed460764fa9c33f
Vilin Kehayov <112933705+VilinKehayov@users.noreply.github.com>
2023-04-19 11:57:17 +0300

    handleDelete function made

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -18,6 +18,17 @@ const Books = () => {
         }
         fetchAllBooks();
     },[]);
+
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete("http://localhost:3000/books/"+id)
+            window.location.reload();
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+    }
   return (
     <div>
         <h1>Vilin's Book Shop</h1>
@@ -28,11 +39,12 @@ const Books = () => {
                 <h2>{book.title}</h2>
                 <p>{book.desc}</p>
                 <span>{book.price}</span>
-                <button className="delete">Delete</button>
-                <button className="update">Update</button>
+                <button className="delete" onClick={()=>handleDelete(book.id)}>Delete</button>
+                <button className="update"><Link to={`/update/${book.id}`}>Update</Link></button>
                </div>
             ))}
         </div>
+        
         <button><Link to="/add">Add new book</Link></button>
     </div>
   )
